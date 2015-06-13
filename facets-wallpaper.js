cc0e3bb9171eeefdb15a2b9f876e2788f5a21f28
Prathyush <kingspprathyush@gmail.com>
2015-06-13 08:32:53 +0530

    Update facets-wallpaper.js

--- facets-wallpaper.js
+++ facets-wallpaper.js
@@ -1,1 +1,31 @@
+var cheerio = require('cheerio'),
+	request = require('request'),
+	fs = require('fs');
 
+var savePath = process.argv[2];
+
+if(typeof savePath === 'undefined') {
+	console.log("Error: Save path not defined.");
+	return;
+}
+
+var year = "2013";
+var baseUri = 'http://www.facets.la/';
+
+(function getImg(id) {
+	var uri = baseUri + year + '/' + id + '/wallpaper/';
+
+	request({ uri: uri }, function(error, response, body) {
+		var $ = cheerio.load(body);
+		var imgDiv = $('#facet-wallpaper').children()['0'];
+
+		if(typeof imgDiv !== 'undefined') {
+			request(imgDiv.attribs.src).pipe(fs.createWriteStream(savePath + id + '.jpg'));
+		}
+	});
+
+	if(id == 330) 
+		year = "2014";
+	if(id <= 365)
+		getImg(id+1);
+})(1);
